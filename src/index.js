4008ed50c443923e1b0a0da9967e473a1db0e1a9
ChristianSama <christiansama1601@gmail.com>
2021-09-12 02:51:37 -0500

    create navbar, add home image and configured webpack output.clean and html plugin

--- src/index.js
+++ src/index.js
@@ -1,43 +1,69 @@
 import Logo from './eagle.png'
-import HomePlate from './gyoza.jpg'
+import HomePlate from './platillo1_cropped.jpg'
 import './style.css'
 
-function loadLogo() {
+function createNavBar() {
+  const nav = document.createElement('nav')
+  nav.innerHTML = 
+    `<ul>
+      <li>
+        HOME
+      </li>
+      <li>
+        ABOUT
+      </li>
+      <li>
+        <div class="brand"></div>
+      </li>
+      <li>
+        MENU
+      </li>
+      <li>
+        CONTACT US
+      </li>
+    </ul>`
+  return nav
+}
+
+function createLogo() {
   const img = new Image()
   img.src = Logo 
   img.classList.add('logo')
   return img
 }
 
-function loadHomePlate() {
+function createHomeImg() {
   const img = new Image()
   img.src = HomePlate
   img.classList.add('home-plate')
   return img
 }
 
-function loadTitle() {
+function createTitle() {
   const title = document.createElement('h1')
   title.textContent = 'La Guarida del Aguila'
   title.classList.add('title')
   return title
 }
 
-function loadHeadline() {
+function createHeadline() {
   const headline = document.createElement('h2')
   headline.textContent = 'El nido de la buena cocina'
   headline.classList.add('headline')
   return headline
 }
 
-function loadDescription() {
+function createDescription() {
   const description = document.createElement('p')
   description.textContent = 'La Guarida del Aguila es un lugar con un ambiente calido y acogedor, con platillos sofisticados y de alta cocina. Es el lugar perfecto para una experiencia refrescante de sabores y sensaciones.'
   description.classList.add('description')
   return description
 }
 
-document.body.appendChild(loadHomePlate())
+document.body.prepend(createNavBar())
+
+const content = document.querySelector('#content')
+content.appendChild(createHomeImg())
 
 const brand = document.querySelector('.brand')
-brand.appendChild(loadLogo())
+brand.appendChild(createLogo())
